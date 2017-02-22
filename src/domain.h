21fef1b37ca22f0c1c3caf2e5250368c0267ccf2
mirko <mirceadino97@gmail.com>
2017-02-22 22:41:39 +0200

    Init bis.

---
+++ src/domain.h
@@ -0,0 +1,8 @@
+#ifndef HASHCODE2017_DOMAIN_H
+#define HASHCODE2017_DOMAIN_H
+
+class Mlc {
+public:
+};
+
+#endif //HASHCODE2017_DOMAIN_H
---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#include "domain.h"
+
+using namespace std;
+
+void read() {
+
+}
+
+void solve() {
+
+}
+
+void print() {
+
+}
+
+int main() {
+    read();
+    solve();
+    print();
+
+    return 0;
+}
